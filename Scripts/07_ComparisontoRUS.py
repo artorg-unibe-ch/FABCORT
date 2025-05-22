Correct for factor 2 in entry 66 and update results

Scripts/07_ComparisontoRUS.py
@@ -29,32 +29,6 @@ from Utils import Read, Tensor
 
 #%% Functions
 
-def TransverseTensor(Lambda0, Mu0, m1, m3):
-
-    m = [m1, m1, m3]
-    m1, m2, m3 = np.eye(3)
-    M1, M2, M3 = np.outer(m1,m1), np.outer(m2,m2), np.outer(m3,m3)
-    M = [M1, M2, M3]
-
-    S4 = np.zeros((3,3,3,3))
-    for i in range(3):
-        S4 += Lambda0 * m[i]**2 * Tensor.Dyadic(M[i],M[i])
-        S4 += Lambda0 * m[i]*m[i-1] * Tensor.Dyadic(M[i],M[i-1])
-        S4 += Lambda0 * m[i]*m[i-1] * Tensor.Dyadic(M[i-1],M[i])
-        S4 +=   Mu0 * m[i]**2 * Tensor.Symmetric(M[i],M[i])
-        S4 +=   2*Mu0 * m[i]*m[i-1] * Tensor.Symmetric(M[i-1],M[i])
-    
-    return S4
-
-def TransverseResiduals(Parameters, m1, m3, A):
-
-    Lambda0, Mu0 = Parameters
-    B = TransverseTensor(Lambda0, Mu0, m1, m3)
-    B = Tensor.IsoMorphism3333_66(B)
-    Residuals = abs(Tensor.Norm(A) - Tensor.Norm(B))
-    
-    return Residuals
-
 def OLS(X,Y):
     XTXi = np.linalg.inv(X.T * X)
     B = XTXi * X.T * Y
@@ -137,7 +111,7 @@ def ExpVsSim_OLS(X, Y, Alpha=0.95, FName=''):
     Axes.annotate(r'N Points : ' + str(len(Y)), xy=(0.3, 0.025), xycoords='axes fraction')
     Axes.annotate(r'$R^2_{ajd}$: ' + format(round(R2adj, 3),'.3f'), xy=(0.65, 0.1), xycoords='axes fraction')
     Axes.annotate(r'NE : ' + format(round(NE.mean(), 2), '.2f') + '$\pm$' + format(round(NE.std(), 2), '.2f'), xy=(0.65, 0.025), xycoords='axes fraction')
-    Axes.annotate(r'y = : ' + format(round(B[0,0],2), '.2f') + ' + ' + format(round(B[1,0], 2), '.2f') + 'x', xy=(0.3, 0.85), xycoords='axes fraction')
+    Axes.annotate(r'y = ' + format(round(B[0,0],2), '.2f') + ' + ' + format(round(B[1,0], 2), '.2f') + 'x', xy=(0.3, 0.85), xycoords='axes fraction')
     Axes.set_ylabel('Simulation $\mathrm{\mathbb{S}}$ (MPa)')
     Axes.set_xlabel('Experiment $\mathrm{\mathbb{S}}$ (MPa)')
     # plt.xscale('log')
@@ -154,8 +128,11 @@ def ExpVsSim_OLS(X, Y, Alpha=0.95, FName=''):
 
 def Main():
 
+    # Read CV data
+    CVData = pd.read_csv(Path(__file__).parents[1] / 'Results' / 'CV.csv', index_col=[0,1])
+
     # Read RUS data
-    DataPath = Path(__file__).parents[1] / '_Data'
+    DataPath = Path(__file__).parents[1] / 'Data'
     RUS = pd.read_csv(DataPath / '00_RUSData.csv')
     RUS['S12'] = RUS['S11'] - 2*RUS['S66']
 
@@ -176,8 +153,8 @@ def Main():
         mRUS[i,5,5] = Row['S66']
 
     # Define paths
-    ResultsPath = Path(__file__).parents[1] / '_Results' / 'Cortical' / 'Homogenisation'
-    FabricPath = Path(__file__).parents[1] / '_Results' /  'Cortical' /'Fabric'
+    ResultsPath = Path(__file__).parents[1] / 'Results' / 'Homogenisation'
+    FabricPath = Path(__file__).parents[1] / 'Results' /'Fabric'
     Folders = sorted([Folder.name for Folder in FabricPath.iterdir() if Folder.is_dir()])
 
     # List simulations output files
@@ -193,10 +170,14 @@ def Main():
     Transverse = np.zeros((len(Folders), len(ROIs), 6, 6))
     Fabric = np.zeros((len(Folders), len(ROIs), 3))
     Rho = np.zeros((len(Folders), len(ROIs)))
+    CV = np.zeros((len(Folders), len(ROIs)))
     for f, Folder in enumerate(Folders):
 
         # Iterate over each ROI
         for r, ROI in enumerate(ROIs):
+
+            # Get sample CV
+            CV[f,r] = CVData.loc[(Folder, ROI),'CV']
 
             # Get MIL results
             FabricData = Read.Fabric(FabricPath / Folder / (ROI+'.fab'))
@@ -234,14 +215,6 @@ def Main():
     mIsotropic = np.mean(Isotropic, axis=1)
     mTransverse = np.mean(Transverse, axis=1)
 
-    # Project tensors onto transverse isotropy
-    Lambda0 = 1E4
-    Mu0 = 2500
-    m1 = (mFabric[0,0] + mFabric[0,1]) / 2
-    m3 = mFabric[0,2]
-    Lambda0, Mu0 = minimize(TransverseResiduals, [Lambda0, Mu0], args=(m1, m3, mTransverse[0])).x
-    Tensor.IsoMorphism3333_66(TransverseTensor(Lambda0, Mu0, m1, m3))
-
     # Plot average ROI BV/TV vs RUS BV/TV
     X = np.ones((len(mRho),2))
     X[:,1] = 1-RUS['Rho']
@@ -251,25 +224,19 @@ def Main():
     X = np.linspace(Min, Max)
     Y = B[0,0] + X * B[1,0]
 
+
     Figure, Axis = plt.subplots(1,1,dpi=200)
     Axis.plot(1-RUS['Rho'], 1-mRho, linestyle='none', color=(1,0,0),
-              marker='o', fillstyle='none', label='Data')
-    Axis.plot(X, Y, color=(0,0,1), linestyle='--', label='Fit')
-    Axis.plot([Min,Max], [Min,Max], color=(0,0,0), linestyle='--', label='1:1 line')
-    Axis.annotate(f'y = {round(B[0,0],2)} + {round(B[1,0],2)} x', (0.25,0.05), color=(0,0,1))
+              marker='o', label='Data')
+    Axis.plot(X, Y, color=(0,0,0), linestyle='--', label='Fit')
+    Axis.plot([Min,Max], [Min,Max], color=(0.4,0.4,0.4), linestyle='--', label='1:1 line')
+    Axis.annotate(f'y = {round(B[0,0],2)} + {round(B[1,0],2)} x', (0.25,0.05), color=(0,0,0))
     Axis.set_xlabel(r'RUS 1-$\rho$ (-)')
     Axis.set_ylabel(r'Average ROI 1-$\rho$ (-)')
     plt.legend()
-    plt.savefig(Path(__file__).parents[1] / '_Results/ExpSim_Rho.png')
+    plt.savefig(Path(__file__).parents[1] / 'Results/ExpSim_Rho.png')
     plt.show(Figure)
 
-    # Filter out sample with BV/TV < 0.7
-    F = mRho > 0.7
-    mIsotropic = mIsotropic[F]
-    mTransverse = mTransverse[F]
-    mRUS = mRUS[F]
-    mRho = mRho[F]
-    
     # Compare with RUS
     X = np.matrix(np.ones((len(mRho)*12, 2)))
     Y = np.matrix(np.zeros((len(mRho)*12, 1)))
@@ -301,31 +268,78 @@ def Main():
                          [mTransverse[f][2,2]],
                          [mTransverse[f][3,3]],
                          [mTransverse[f][4,4]],
-                         [mTransverse[f][5,5]]]
+                         [mTransverse[f][5,5]/2]]
        
-    FName = Path(__file__).parents[1] / '_Results/ExpSim_S.png'
+    FName = Path(__file__).parents[1] / 'Results/ExpSim_S_Raw.png'
     Parameters, R2adj, NE = ExpVsSim_OLS(X, Y/1E3, FName=str(FName))
 
+    # Project tensors onto transverse isotropy
+    mProjected, NE = Tensor.TransProj(mTransverse)
+
+    # Compare with RUS
+    X = np.matrix(np.ones((len(mRho)*12, 2)))
+    Y = np.matrix(np.zeros((len(mRho)*12, 1)))
+    for f in range(len(mRho)):
+        
+        Start, Stop = 12*f, 12*(f+1)
+        
+        X[Start:Stop,1] = [[mRUS[f][0,0]/1E3],
+                           [mRUS[f][0,1]/1E3],
+                           [mRUS[f][0,2]/1E3],
+                           [mRUS[f][1,0]/1E3],
+                           [mRUS[f][1,1]/1E3],
+                           [mRUS[f][1,2]/1E3],
+                           [mRUS[f][2,0]/1E3],
+                           [mRUS[f][2,1]/1E3],
+                           [mRUS[f][2,2]/1E3],
+                           [mRUS[f][3,3]/1E3],
+                           [mRUS[f][4,4]/1E3],
+                           [mRUS[f][5,5]/1E3]]
+        
+        Y[Start:Stop] = [[mProjected[f][0,0]],
+                         [mProjected[f][0,1]],
+                         [mProjected[f][0,2]],
+                         [mProjected[f][1,0]],
+                         [mProjected[f][1,1]],
+                         [mProjected[f][1,2]],
+                         [mProjected[f][2,0]],
+                         [mProjected[f][2,1]],
+                         [mProjected[f][2,2]],
+                         [mProjected[f][3,3]],
+                         [mProjected[f][4,4]],
+                         [mProjected[f][5,5]/2]]
+       
+    FName = Path(__file__).parents[1] / 'Results/ExpSim_S_Proj.png'
+    Parameters, R2adj, NE = ExpVsSim_OLS(X, Y/1E3, FName=str(FName))
+
+    # Filter out samples with high CV
+    F = np.all(CV < 0.263, axis=1)
+    F12 = np.repeat(F, 12)
+    FName = Path(__file__).parents[1] / 'Results/ExpSim_S_Filt.png'
+    Parameters, R2adj, NE = ExpVsSim_OLS(X[F12], Y[F12]/1E3, FName=str(FName))
+    
+
+
     # Compare degree of anisotropy
-    X = np.ones((len(mRho),2))
-    X[:,1] = 1-mRho
-    Args = np.argsort(1-mRho)
+    X = np.ones((len(mRho[F]),2))
+    X[:,1] = 1-mRho[F]
+    Args = np.argsort(1-mRho[F])
     Colors = [(0,0,1),(1,0,0)]
     Labels = ['Experiment','Simulation']
 
     Figure, Axis = plt.subplots(1,1,dpi=200)
-    for i, (Y, C, L) in enumerate(zip([mRUS, mTransverse], Colors, Labels)):
+    for i, (Y, C, L) in enumerate(zip([mRUS[F], mTransverse[F]], Colors, Labels)):
         Y = Y[:,2,2] / Y[:,0,0]
         B = OLS(np.matrix(X),np.matrix(Y).T)
-        Axis.plot(1-mRho, Y, color=C, label=L,
+        Axis.plot(1-mRho[F], Y, color=C, label=L,
                 marker='o', fillstyle='none', linestyle='none')
-        Axis.plot((1-mRho)[Args], B[0,0] + (1-mRho)[Args]*B[1,0], color=C, linestyle='--')
+        Axis.plot((1-mRho[F])[Args], B[0,0] + (1-mRho[F])[Args]*B[1,0], color=C, linestyle='--')
         Axis.annotate(f'y = {round(B[0,0],2)} + {round(B[1,0],2)}x', color=C,
                       xy=(0.0475, 1.76-i*0.05))
     Axis.set_xlabel(r'1-$\rho$ (-)')
     Axis.set_ylabel(r'$S_{33}$ / $S_{11}$ (-)')
     plt.legend()
-    plt.savefig(Path(__file__).parents[1] / '_Results/ExpSim_AniS.png')
+    plt.savefig(Path(__file__).parents[1] / 'Results/ExpSim_AniS.png')
     plt.show(Figure)
 
     # Same for compliance
@@ -337,25 +351,25 @@ def Main():
     for i, S in enumerate(mRUS):
         E_RUS[i] = np.linalg.inv(S)
 
-    X = np.ones((len(mRho),2))
-    X[:,1] = 1-mRho
-    Args = np.argsort(1-mRho)
+    X = np.ones((len(mRho[F]),2))
+    X[:,1] = 1-mRho[F]
+    Args = np.argsort(1-mRho[F])
     Colors = [(0,0,1),(1,0,0)]
     Labels = ['Experiment','Simulation']
 
     Figure, Axis = plt.subplots(1,1,dpi=200)
-    for i, (Y, C, L) in enumerate(zip([E_RUS, E_Sim], Colors, Labels)):
+    for i, (Y, C, L) in enumerate(zip([E_RUS[F], E_Sim[F]], Colors, Labels)):
         Y = Y[:,0,0] / Y[:,2,2]
         B = OLS(np.matrix(X),np.matrix(Y).T)
-        Axis.plot(1-mRho, Y, color=C, label=L,
+        Axis.plot(1-mRho[F], Y, color=C, label=L,
                 marker='o', fillstyle='none', linestyle='none')
-        Axis.plot((1-mRho)[Args], B[0,0] + (1-mRho)[Args]*B[1,0], color=C, linestyle='--')
+        Axis.plot((1-mRho[F])[Args], B[0,0] + (1-mRho[F])[Args]*B[1,0], color=C, linestyle='--')
         Axis.annotate(f'y = {round(B[0,0],2)} + {round(B[1,0],2)}x', color=C,
                       xy=(0.2, 1.45-i*0.05))
     Axis.set_xlabel(r'1-$\rho$ (-)')
     Axis.set_ylabel(r'$E_{33}$ / $E_{11}$ (-)')
     plt.legend()
-    plt.savefig(Path(__file__).parents[1] / '_Results/ExpSim_AniE.png')
+    plt.savefig(Path(__file__).parents[1] / 'Results/ExpSim_AniE.png')
     plt.show(Figure)
 
 